594f1e76fc78a0d8769a4df8e527fd61c710cb3b
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-06-14 02:40:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Longest consecutive sequence in Binary tree/longest-consecutive-sequence-in-binary-tree.java
@@ -0,0 +1,27 @@
+class Solution {
+    int count = 0;
+
+    public int longestConsecutive(Node root) {
+        dfs(root);
+        return count < 2 ? -1 : count;
+    }
+
+    int dfs(Node root) {
+        if (root == null) return 0;
+
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        int leftcount = 1, rightcount = 1;
+
+        if (root.left != null && root.left.data == root.data + 1) {
+            leftcount = left + 1;
+        }
+        if (root.right != null && root.right.data == root.data + 1) {
+            rightcount = right + 1;
+        }
+
+        int temp = Math.max(leftcount, rightcount);
+        count = Math.max(count, temp);
+        return temp;
+    }
+}
