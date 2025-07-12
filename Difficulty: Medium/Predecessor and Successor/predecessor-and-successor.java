9b6bf24ef5f4e62585e5c8188e0a6f10dea2af85
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-07-12 18:29:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Predecessor and Successor/predecessor-and-successor.java
+++ Difficulty: Medium/Predecessor and Successor/predecessor-and-successor.java
@@ -1,47 +1,51 @@
+
 class Solution {
+    Node predecessor=null;
+    Node successor=null;
+        
     public ArrayList<Node> findPreSuc(Node root, int key) {
-        ArrayList<Node> list = new ArrayList<>();
-        ArrayList<Node> result = new ArrayList<>();
-
-        inorder(root, list);
-
-        int low = 0, high = list.size() - 1;
-        Node predecessor = null;
-        Node successor = null;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int val = list.get(mid).data;
-
-            if (val == key) {
-                if(mid>0){
-                    predecessor=list.get(mid-1);
-                }
-                if(mid<list.size()-1){
-                    successor=list.get(mid+1);
-                    
-                }
-                break;
-            }
-            else if (val > key) {
-                successor = list.get(mid);
-                high = mid - 1;
-            } else {
-                predecessor = list.get(mid);
-                low = mid + 1;
-            }
-        }
-
+        
+        ArrayList<Node>result=new ArrayList<>();
+        
+        inorder(root,key);
+        
         result.add(predecessor);
         result.add(successor);
-
+        
         return result;
+        
     }
-
-    void inorder(Node root, List<Node> list) {
-        if (root == null) return;
-        inorder(root.left, list);
-        list.add(root);
-        inorder(root.right, list);
+    void inorder(Node root,int key){
+        if(root==null){
+            return;
+        }
+        if(root.data==key){
+            if(root.left!=null){
+                Node temp=root.left;
+                while(temp.right!=null){
+                    temp=temp.right;
+                }
+                predecessor=temp;
+            }
+            
+            if(root.right!=null){
+                Node temp=root.right;
+                while(temp.left!=null){
+                    temp=temp.left;
+                }
+                successor=temp;
+            }
+            
+        }
+        else if(root.data>key){
+            successor=root;
+            inorder(root.left,key);
+            
+        }
+        else{
+            predecessor=root;
+            inorder(root.right,key);
+        }
+        return;
     }
 }
