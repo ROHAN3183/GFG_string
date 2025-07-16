39cd5392bbd546bf49a108222d22c538bcfe7e7c
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-07-16 20:20:47 +0530

    Update earliest-common-slot.java

--- Difficulty: Medium/Earliest Common Slot/earliest-common-slot.java
+++ Difficulty: Medium/Earliest Common Slot/earliest-common-slot.java
@@ -1,3 +1,31 @@
+*******************************************************BRUTE FORCE***************************************************************
+class Solution {
+    public int[] commonSlot(List<int[]> a, List<int[]> b, int d) {
+        
+        int n2 = b.size();
+        int n1 = a.size();
+        
+        for (int i = 0; i < n2; i++) {
+            
+            int keyStart = b.get(i)[0];
+            int keyEnd = b.get(i)[1];
+            
+            for (int j = 0; j < n1; j++) {
+                int start = Math.max(keyStart, a.get(j)[0]);
+                int end = Math.min(keyEnd, a.get(j)[1]);
+                
+                if(end-start>=d){
+                    return new int[]{start,start+d};
+                }    
+                
+                
+            }
+        }
+        return new int[] {};
+    }
+}
+/* LOGIC of the code -> if the ending is small it means that it never come into the future use  so move that pointer*/
+********************************************************OPTIMIZED CODE**********************************************************
 class Solution {
     public int[] commonSlot(List<int[]> a, List<int[]> b, int d) {
         
