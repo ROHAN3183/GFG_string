1573ebbfc464b97ae3b2dfad7f06fbf2dc8d6362
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-07-18 15:44:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.java
@@ -0,0 +1,48 @@
+class Solution {
+    int median(int mat[][]) {
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        int N= mat.length * mat[0].length;
+        
+        for(int i=0 ;i<mat.length;i++){
+            min=Math.min(mat[i][0],min);
+            max=Math.max(mat[i][mat[0].length-1],max);
+        }
+        int result=-1;
+        int low=min;
+        int high=max;
+       
+        while(low<=high){
+            int mid=low+(high-low)/2;
+             int length=0;
+            for(int i=0 ;i< mat.length ;i++){
+                length+=findtheSmallestCount(mat[i],mid);
+            }
+            if(length<=N/2){
+                low=mid+1;
+            }
+            else{
+                result=mid;
+                high=mid-1;
+            }
+        }
+        return result;
+    
+    }
+    int  findtheSmallestCount(int mat[],int target){
+        int low=0;
+        int high=mat.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            
+            if(mat[mid]<=target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        
+        return low;
+    }
+}
