887a2eb76c073e4b15e5500bc3db90c1e2d22356
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-07-19 14:56:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Row with Minimum 1s/row-with-minimum-1s.java
+++ Difficulty: Easy/Row with Minimum 1s/row-with-minimum-1s.java
@@ -2,27 +2,38 @@ class Solution {
     int minRow(int mat[][]) {
         int row=mat.length;
         int col=mat[0].length;
-        int minIndex=-1;
-        int countOfOnes=Integer.MAX_VALUE;
+        int index=-1;
+        int minCount=Integer.MAX_VALUE;
         
         for(int i=0;i<row;i++){
-            int maxCountOnes=0;
-            for(int j=0;j<col;j++){
-                
-                if(mat[i][j]==1){
-                    maxCountOnes++;
-                }
-                
-            }
-            
-            if(maxCountOnes < countOfOnes){
-                countOfOnes=maxCountOnes;
-                minIndex=i;
-            }
-            
+            Arrays.sort(mat[i]);
         }
+        //binary Search
+        for(int i=0;i<row;i++){
+            int count=countOnes(mat[i]);
+            if(count<minCount){
+                minCount=count;
+                index=i;
+            }
+        }
+        return index+1;
         
-        return minIndex+1;
-        
+    }
+    int countOnes(int[]mat){
+        int low=0;
+        int high=mat.length-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(mat[mid]>=1){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        if (ans == -1) return 0; 
+        return mat.length - ans;
     }
 }
