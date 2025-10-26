1075dbbeb2cb44ef16ed0ac2705e0b779af0b7ac
Rohan Thakur <rohan.thakur2023@vitstudent.ac.in>
2025-10-26 10:52:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.java
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.java
@@ -1,53 +1,25 @@
 class Solution {
-    class Pair {
-        int node;
-        int dis;
-
-        Pair(int node, int dis) {
-            this.node = node;
-            this.dis = dis;
-        }
-    }
-
     public int[] dijkstra(int V, int[][] edges, int src) {
-        int[] distance = new int[V];
-        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-        Arrays.fill(distance, Integer.MAX_VALUE);
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> {
-            if (a.dis == b.dis) {
-                return a.node - b.node;
-            }
-            return a.dis - b.dis;
-        });
-        for (int i = 0; i < V; i++) {
-            adj.add(new ArrayList<>());
-        }
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            int w = edge[2];
-            adj.get(u).add(new Pair(v, w));
-            adj.get(v).add(new Pair(u, w));
-        }
-        return dijkstraLogic(adj, distance, pq, src);
-    }
-
-    int[] dijkstraLogic(ArrayList<ArrayList<Pair>> adj, int[] distance, PriorityQueue<Pair> pq, int src) {
-        distance[src] = 0;
-        pq.offer(new Pair(src, 0));
-        while (!pq.isEmpty()) {
-            Pair curr = pq.poll();
-            int currNode = curr.node;
-            int currDis = curr.dis;
-            if (currDis > distance[currNode]) {
-                continue;
-            }
-            for (Pair neibour : adj.get(currNode)) {
-                if (currDis + neibour.dis < distance[neibour.node]) {
-                    distance[neibour.node] = currDis + neibour.dis;
-                    pq.offer(new Pair(neibour.node, distance[neibour.node]));
+        int [] distance = new int [V];
+        Arrays.fill(distance,Integer.MAX_VALUE);
+        distance[src]=0;
+        
+        for(int i=0;i<V-1;i++){
+            boolean updated=false;
+            for(int [] edge:edges){
+                int u=edge[0];
+                int v=edge[1];
+                int w=edge[2];
+                if(distance[u]!=Integer.MAX_VALUE &&distance[u]+w<distance[v]){
+                    distance[v]=distance[u]+w;
+                    updated=true;
+                }
+                if(distance[v]!=Integer.MAX_VALUE &&distance[v]+w<distance[u]){
+                    distance[u]=w+distance[v];
+                    updated=true;
                 }
             }
+            if(!updated)break;
         }
         return distance;
     }
